9295783e8c8e2a98a869f5bbafa79c6ba96ad8cc
albertogaia <alberto.gaia95@gmail.com>
2021-10-11 15:54:08 +0200

    Finito con header

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
         [
             'domanda' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
             'risposta' => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
-
             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
-            
             Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.
-
             Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione. "
             
@@ -28,8 +25,8 @@
             <ol><li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
                 <ol><li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
                     <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
-                </ol>
-            </li><li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li></ol>
+                </ol></li>
+            <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li></ol>
             Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
             <h3>Stabilire il paese associato al tuo account</h3>
             Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
@@ -46,7 +43,33 @@
             'risposta'=>"In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
         ]
     ];
-
+    $navLinks = [
+        [
+            'title' => 'Introduzione',
+            'url' => '#',
+            'status'=>'',
+        ],
+        [
+            'title' => 'Norme sulla privacy',
+            'url' => '#',
+            'status'=>'',
+        ],
+        [
+            'title' => 'Termini di servizio',
+            'url' => '#',
+            'status'=>'',
+        ],
+        [
+            'title' => 'Tecnologie',
+            'url' => '#',
+            'status'=>'',
+        ],
+        [
+            'title' => 'Domande frequenti',
+            'url' => '#',
+            'status'=>'active',
+        ]
+    ]
 ?>
 
 <!DOCTYPE html>
@@ -62,6 +85,24 @@
     <title>Domande Frequenti</title>
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <img src="https://1000marche.net/wp-content/uploads/2020/03/Google-logo.png" alt="logo Google"> <span class="logo-text">Privacy e termini</span>
+        </div>
+        <div class="nav-menu">
+            <ul>
+                <?php
+                    foreach($navLinks as $link){
+                        if($link['status'] === 'active'){
+                            echo "<li><a href=".$link['url']." class='active'>".$link['title']."</a></li>";
+                        }else{
+                            echo "<li><a href=".$link['url'].">".$link['title']."</a></li>";
+                        }
+                    }
+                ?>
+            </ul>
+        </div>
+    </header>
     <div class="container">
             <?php
                 foreach($faqs as $faq){
